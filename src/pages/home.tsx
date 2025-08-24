0665d4faf66e46983d81ed2a82b3eeb8d6a14d2f
Raheem Khan <raheemkhan@Raheems-MacBook-Pro.local>
2025-08-24 21:19:34 +0100

    Add new sections to homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 
 // Define props for the Home component, including a function to change the page
 interface HomeProps {
